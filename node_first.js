c742de3a667e085826fd1c39c55876b5896ccfd9
Ankur <ankur@Ankurs-iMac.local>
2021-07-21 18:24:19 +0530

    connection btw node and frontend done

--- node_first.js
+++ node_first.js
@@ -1,61 +1,92 @@
-// const http = require('http');
-
-// const hostname = '127.0.0.1';
-// const port = 4000;
-
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/plain');
-//   // res.setHeader('Access-Control-Allow-Origin', '*');
-//   // res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   res.end('Hello World');
-// });
-
-// server.listen(port, hostname, () => {
-//   console.log(`Server running at http://${hostname}:${port}/`);
-// });
-
+var express = require('express');
+var app = express();
 var MongoClient = require('mongodb').MongoClient;
 var url = 'mongodb://127.0.0.1:27017/EmployeeDB';
+var str = "";
 
-MongoClient.connect(url, function(err, client) {
+// app.route('/Employeeid').get(function(req, res) {
+//   MongoClient.connect(url, function(err, db) {
+//       var collection = db.collection('Employee');
+//       var cursor = collection.find({});
+//       str = "";
+//       cursor.forEach(function(item) {
+//           if (item != null) {
+//                   str = str + "    Employee id  " + item.Employeeid + "</br>";
+//           }
+//       }, function(err) {
+//           res.send(err);
+//           db.close();
+//          }
+//       );
+//   });
+// });
+
+app.route('/Employeeid').get(function (req, res) {
+  console.log("inside route");
+  MongoClient.connect(url, function (err, client) {
+    str = "";
+    console.log("connected to mongo");
+
     var db = client.db('EmployeeDB');
-    
-//read
-    db.collection('Employee').find({}).toArray(function(err, docs) {
-        docs.forEach(function(doc) {
-          console.log(doc);
-        });
-      client.close();
+
+    db.collection('Employee').find({}).toArray(function (err, docs) {
+      docs.forEach(function (doc) {
+        if (doc != null) {
+          console.log("1st itr: inside foreach loop");
+          str = str + "    Employee id  " + doc.Employeeid + "</br>";
+          console.log(str);
+        }
+      }, function (err) {
+        res.send(err);
+        db.close();
+      }
+      );
+      console.log("second itr: " + str);
+    res.send(str);
     });
+    
+  });
+});
 
-// create, 
-      // db.collection('Employee').insertOne({
-      //   Employeeid: 4,
-      //   EmployeeName: "NewEmployee"
-      // });
+var server = app.listen(8080, () => {
+  console.log("Server running at localhost:8080");
+});
+// server.listen(port, hostname, () => {
+    // console.log(`Server running at http://${hostname}:${port}/`);
+  // });
 
+// app.route('/Employeeid').get(function (req, res) {
+//   MongoClient.connect(url, function (err, client) {
+    // var db = client.db('EmployeeDB');
 
-// update
-        // db.collection('Employee').updateOne({
-        //   "EmployeeName": "NewEmployee"
-        // }, {
-        //   $set: {
-        //       "EmployeeName": "Mohan"
-        //   }
-        // });
+//     //READ
+    // db.collection('Employee').find({}).toArray(function (err, docs) {
+    //   docs.forEach(function (doc) {
+    //     console.log(doc);
+    //   });
+//       client.close();
+//     });
 
+//     // CREATE, 
+//     // db.collection('Employee').insertOne({
+//     //   Employeeid: 4,
+//     //   EmployeeName: "NewEmployee"
+//     // });
 
-// delete
-          // db.collection('Employee').deleteOne(
+//     // UPDATE
+//     // db.collection('Employee').updateOne({
+//     //   "EmployeeName": "NewEmployee"
+//     // }, {
+//     //   $set: {
+//     //       "EmployeeName": "Mohan"
+//     //   }
+//     // });
 
-          //   {
-          //       "EmployeeName": "Mohan"
-          //   }
-
-          // );
-
-
-}); 
-
-
+//     // DELETE
+//     // db.collection('Employee').deleteOne(
+//     //   {
+//     //       "EmployeeName": "Mohan"
+//     //   }
+//     // );
+//   });
+// });
